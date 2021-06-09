1ef9fb7ba64999e180c0196e9d1193afb76ad924
Aanandi03 <67504112+Aanandi03@users.noreply.github.com>
2021-06-09 18:05:36 +0530

    v1

---
+++ Day_4/funcWithReturn.py
@@ -0,0 +1,7 @@
+def full_name():
+    name = 'Aanandi'
+    surname = 'Rajpara'    
+    return name, surname
+
+name,surname = full_name()
+print('full name: ',name,' ',surname) 
---
+++ Day_4/function.py
@@ -0,0 +1,5 @@
+def print_name ():
+    print('Aanandi Rajpara')
+
+print_name()
+print_name()
